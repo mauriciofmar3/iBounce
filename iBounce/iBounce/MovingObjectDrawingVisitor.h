7a8d3830f056b8f6acae155f37e98c1c5316b8c9
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2014-05-09 20:27:48 -0400

    Visitor for drawing balls or squares

---
+++ iBounce/iBounce/MovingObjectDrawingVisitor.h
@@ -0,0 +1,13 @@
+//
+//  MovingObjectDrawingVisitor.h
+//  iBounce
+//
+//  Created by Mauricio Bell'Albero on 5/9/14.
+//  Copyright (c) 2014 com.maumau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MovingObjectDrawingVisitor : NSObject
+
+@end
---
+++ iBounce/iBounce/MovingObjectDrawingVisitor.m
@@ -0,0 +1,13 @@
+//
+//  MovingObjectDrawingVisitor.m
+//  iBounce
+//
+//  Created by Mauricio Bell'Albero on 5/9/14.
+//  Copyright (c) 2014 com.maumau. All rights reserved.
+//
+
+#import "MovingObjectDrawingVisitor.h"
+
+@implementation MovingObjectDrawingVisitor
+
+@end
