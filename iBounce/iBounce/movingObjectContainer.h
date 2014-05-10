360c41e4b5c22ecfdeb63f213437e2d7e42836d4
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2014-05-09 20:01:53 -0400

    Container for the movingObject

---
+++ iBounce/iBounce/movingObjectContainer.h
@@ -0,0 +1,16 @@
+//
+//  movingObjectContainer.h
+//  iBounce
+//
+//  Created by Mauricio Bell'Albero on 5/9/14.
+//  Copyright (c) 2014 com.maumau. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "MovingObject.h"
+
+@interface movingObjectContainer : NSObject
+
+@property (nonatomic) MovingObject* movingObject;
+
+@end
