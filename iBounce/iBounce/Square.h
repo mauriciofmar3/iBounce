98eb85eecefdd712b449da090587696235daeb83
Mauricio E. Bell'Albero <mauriciofmar3@gmail.com>
2014-05-13 20:38:28 -0400

    Square ball

---
+++ iBounce/iBounce/Square.h
@@ -0,0 +1,13 @@
+//
+//  Square.h
+//  iBounce
+//
+//  Created by Mauricio Bell'Albero on 5/13/14.
+//  Copyright (c) 2014 com.maumau. All rights reserved.
+//
+
+#import "MovingObject.h"
+
+@interface Square : MovingObject
+
+@end
---
+++ iBounce/iBounce/Square.m
@@ -0,0 +1,13 @@
+//
+//  Square.m
+//  iBounce
+//
+//  Created by Mauricio Bell'Albero on 5/13/14.
+//  Copyright (c) 2014 com.maumau. All rights reserved.
+//
+
+#import "Square.h"
+
+@implementation Square
+
+@end
